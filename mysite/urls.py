cd12f68ceb839d0951483618e27247cef0968aba
Oliunin <55708721+Oliunin@users.noreply.github.com>
2019-09-26 22:10:59 +0500

    views and models

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+#    path('lk/',hello),
+
 ]
